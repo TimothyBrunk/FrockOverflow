80e059070d5e85c92065f01a9425273d56179582
Timothy Brunk <Timothy.Brunk@gmail.com>
2016-04-28 13:17:33 -0600

    Merging conflicts

--- FrockOverflowJPA/src/entities/Answer.java
+++ FrockOverflowJPA/src/entities/Answer.java
@@ -1,10 +1,12 @@
 package entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
+
 public class Answer {
-	
+	@Id
 	private int id;
 	private int userId;
 	private String body;
