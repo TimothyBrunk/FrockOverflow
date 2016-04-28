3697b7e3f41d761e1322c254907b81426fd9d6dd
sarahlobser <sarah.lobser@gmail.com>
2016-04-28 11:58:38 -0600

    Adding practice SQL database

---
+++ SQL/frockoverflowdb.sql
@@ -0,0 +1,98 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema frockoverflowdb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema frockoverflowdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `frockoverflowdb` DEFAULT CHARACTER SET utf8 ;
+USE `frockoverflowdb` ;
+
+-- -----------------------------------------------------
+-- Table `frockoverflowdb`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `frockoverflowdb`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `frockoverflowdb`.`user` (
+  `id` INT NOT NULL,
+  `firstname` VARCHAR(45) NULL,
+  `lastname` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  `type` TINYINT(2) NOT NULL,
+  `display_name` VARCHAR(45) NOT NULL,
+  `date_created` DATE NOT NULL,
+  `password` VARCHAR(45) NULL,
+  `answer_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `email_UNIQUE` ON `frockoverflowdb`.`user` (`email` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `frockoverflowdb`.`question`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `frockoverflowdb`.`question` ;
+
+CREATE TABLE IF NOT EXISTS `frockoverflowdb`.`question` (
+  `id` INT NOT NULL,
+  `time_stamp` DATETIME NOT NULL,
+  `body` VARCHAR(45) NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `rating` INT NULL,
+  `views` INT NULL,
+  `flag` VARCHAR(45) NULL,
+  `user_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `frockoverflowdb`.`answer`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `frockoverflowdb`.`answer` ;
+
+CREATE TABLE IF NOT EXISTS `frockoverflowdb`.`answer` (
+  `id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `rating` INT NULL,
+  `answer_status` VARCHAR(45) NULL,
+  `time_stamp` DATETIME NULL,
+  `flag` VARCHAR(45) NULL,
+  `answer_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `frockoverflowdb`.`questiontags`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `frockoverflowdb`.`questiontags` ;
+
+CREATE TABLE IF NOT EXISTS `frockoverflowdb`.`questiontags` (
+  `id` INT NOT NULL,
+  `question_id` VARCHAR(45) NULL,
+  `tag_id` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `frockoverflowdb`.`user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `frockoverflowdb`;
+INSERT INTO `frockoverflowdb`.`user` (`id`, `firstname`, `lastname`, `email`, `type`, `display_name`, `date_created`, `password`, `answer_id`) VALUES (1001, 'Sarah', 'Lobser', 'sarah.lobser@gmail.com', 1, 'lobes', NOW(), 'stub', NULL);
+
+COMMIT;
+
