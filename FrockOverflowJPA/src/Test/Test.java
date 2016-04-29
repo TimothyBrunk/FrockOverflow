3539d2fa2a278ca214a8ff33a2b33ae573e200ed
sarahlobser <sarah.lobser@gmail.com>
2016-04-29 08:42:19 -0600

    edited Test.java

--- FrockOverflowJPA/src/Test/Test.java
+++ FrockOverflowJPA/src/Test/Test.java
@@ -4,8 +4,10 @@ import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import entities.Answer;
 import entities.Question;
 import entities.User;
 
@@ -17,16 +19,35 @@ public class Test {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("frockoverflowdb");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		User u = em.find(User.class, 1002);
-		Question a = new Question();
-		String sb = "What is the default value of float and double datatype in Java?";
-		a.setBody(sb);
-		a.setTimestamp(new Date());
+		EntityTransaction et = em.getTransaction();
+		et.begin();
+		User u = em.find(User.class, 1003);
+		
+		Question q = new Question();
+		String sb = "Where does snow come from, and why can't I make it using this script?";
+		q.setBody(sb);
+		q.setTimestamp(new Date());
 		System.out.println(u);
+		q.setUser(u);
+		
+		em.persist(q);
+		
+		
+//		Question q = em.find(Question.class, 4);
+		System.out.println(q.getUser());
+		
+		Answer a = new Answer();
+		a.setBody("Take a nap");
 		a.setUser(u);
+		a.setQuestion(q);
+		
 		em.persist(a);
-		em.getTransaction().commit();
+		
+		em.remove(q);
+		et.commit();
+		
+//		em.persist(q);
+//		em.getTransaction().commit();
 		
 		
 		em.close();
