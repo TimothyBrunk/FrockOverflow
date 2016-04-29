e1140823f3331f47dda7397af98cf5e0f59dca53
Brenton Gumucio <Brenton.gumucio@gmail.com>
2016-04-29 10:42:28 -0600

    Added getQuestion method to DAO

--- FrockOverflowDWP/src/data/FrockOverflowDBDAO.java
+++ FrockOverflowDWP/src/data/FrockOverflowDBDAO.java
@@ -5,9 +5,18 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import entities.Question;
+
 @Transactional
 public class FrockOverflowDBDAO implements FrockOverflowDao {
 	@PersistenceContext
 	private EntityManager em;
+	
+	public Question getQuestion(int id){
+		Question q = em.find(Question.class, id);
+		em.detach(q);
+		return q;
+		
+	}
 
 }
