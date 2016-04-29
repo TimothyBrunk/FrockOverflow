981d84136ff761c2ddd015a873972909453f4026
Timothy Brunk <Timothy.Brunk@gmail.com>
2016-04-29 14:01:35 -0600

    Added Session Attribute to Controller, Added CreateQuestion Controller method, Some visiual changes to index.jsp.

--- FrockOverflowDWP/src/controller/FrockOverflowController.java
+++ FrockOverflowDWP/src/controller/FrockOverflowController.java
@@ -1,18 +1,29 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import data.FrockOverflowDao;
 import entities.Question;
+import entities.User;
 
 @Controller
+@SessionAttributes("user")
 public class FrockOverflowController {
 	 @Autowired
 	    private FrockOverflowDao frockoverflowdao;
+	 
+	 @ModelAttribute("user")
+	 public User initUser() {
+		 return new User(); 
+	 }
 	    
 		@RequestMapping("GetQuestionByID.do")
 		public ModelAndView getQuestion(@RequestParam("id")int id){
@@ -22,9 +33,10 @@ public class FrockOverflowController {
 			mv.addObject("question", q); 
 			return mv;
 		}
+		
 		@RequestMapping("CreateQuestion.do")
 		public ModelAndView getCreateQuestion(Question question){
-			Question c = frockoverflowdao.createQuestion(question); 
+			List <Question> c = frockoverflowdao.createQuestion(question); 
 			ModelAndView mv = new ModelAndView();
 			mv.setViewName("results.jsp");
 			mv.addObject("create", c); 
